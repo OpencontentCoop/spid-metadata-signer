8fa821e24cce35394f10e5d56a2afb21d3132e98
lorello <lorenzo.salvadorini@opencontent.it>
2021-03-23 19:33:45 +0100

    Adeded v3 of the tool and a minimal support for env vars

--- spidMetadataSigner.sh
+++ spidMetadataSigner.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
 set -e 
+[[ -n $DEBUG ]] && set -x
 
 echo -e "\n"
 echo "==============================================="
-echo "      AgID Agenzia per l'Italia Digitale       "
-echo "     Presidenza del Consiglio dei Ministri     "
+echo "              Developers Italia                "
 echo "==============================================="
 echo "  SPID Sistema Pubblico di Identità Digitale   "
 echo "==============================================="
 echo "           SPID Metadata Signer v1.0           "
+echo "             community edition :-)             "
 echo "==============================================="
 echo -e "\n"
 
+
 # Nome del file metadata
-while [ -z "$metadataFileName" ]
+metadataFileName=$METADATA
+while [ -z "${metadataFileName}" ]
 do
-    read -p "> Digita il nome del metadata da firmare (senza estensione es: .xml): " metadataFileName
+    read -p "> Digita il pathname del metadata da firmare (es: /data/metadata.xml): " metadataFileName
 done
 echo -e "\n"
 
 # Nome della chiave
-while [ -z "$keyName" ]
+keyName=$KEY
+while [ -z "${keyName}" ]
 do
-    read -p "> Digita il nome della chiave (con estensione es: .key): " keyName
+    read -p "> Digita il pathname della chiave (con estensione es: ./my-spid.key): " keyName
 done
 echo -e "\n"
 
@@ -35,54 +39,21 @@ fi
 echo -e "\n"
 
 # Nome del certificato
-while [ -z "$crtName" ]
+crtName=$CRT
+while [ -z "${crtName}" ]
 do
-    read -p "> Digita il nome del certificato (con estensione es: .crt): " crtName
+    read -p "> Digita il pathname del certificato (con estensione es: /tmp/my-cert.pem): " crtName
 done
 echo -e "\n"
 
-# Controllo se JAVA è installato
-if type -p java; then
-    echo -e "Java installato sul sistema\n"
-else
-    echo -e "Java non installato sul sistema. Installarlo e riprovare!\n"
-    exit 1
-fi
-
-# Controllo se Unzip è installato
-if type -p unzip; then
-    echo -e "Unzip installato sul sistema\n"
-else
-    echo -e "Unzip non installato sul sistema. Installarlo e riprovare!\n"
-    exit 1
-fi
-
-# Controllo JAVA_HOME
-if [ -z "$JAVA_HOME" ]; then
-    if [ "$(uname)" == "Darwin" ]; then
-        javaHomeTip=$(/usr/libexec/java_home)
-    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-        javaHomeTip=$(dirname $(dirname $(readlink -f $(which javac))))
-    fi
-    # Impostazione del JAVA_HOME
-    while [ -z "$javaHome" ]
-    do
-        read -p "> Digita il JAVA_HOME (suggerimento: $javaHomeTip): " javaHome
-    done
-    export JAVA_HOME=$javaHome
-fi
-echo -e "\n"
-
-# Download e installazione XmlSecTool 2.0.0
-if [ ! -d "xmlsectool-2.0.0" ]; then
-    echo "Scaricamento XmlSecTool 2.0.0:"
-    curl -OJ https://shibboleth.net/downloads/tools/xmlsectool/latest/xmlsectool-2.0.0-bin.zip
-    unzip -qq xmlsectool-2.0.0-bin.zip
-    rm -f xmlsectool-2.0.0-bin.zip
-    echo -e "\n"
-fi
-
 # Firma del metadata
-echo "Firma del metadata: metadata/metadata-in/$metadataFileName.xml"
-./xmlsectool-2.0.0/xmlsectool.sh --sign --referenceIdAttributeName ID --inFile "metadata/metadata-in/$metadataFileName.xml" --outFile "metadata/metadata-out/$metadataFileName-signed.xml" --digest SHA-256 --signatureAlgorithm http://www.w3.org/2001/04/xmldsig-more#rsa-sha256 --key "certs/$keyName" $commandPass --certificate "certs/$crtName"
+echo "Firma del metadata: $metadataFileName"
+echo "Il file firmato verrà salvato in ${metadataFileName}-signed"
+
+./xmlsectool-3.0.0/xmlsectool.sh --sign --referenceIdAttributeName ID \
+  --inFile "$metadataFileName" --outFile "$metadataFileName-signed" \
+  --digest SHA-256 --signatureAlgorithm http://www.w3.org/2001/04/xmldsig-more#rsa-sha256 \
+  --keyFile "$keyName" $commandPass --certificate "$crtName"
+
 echo -e "\n"
+
